67548a594e1802d2b501b509a41c018b7b2698d0
Francisco Front <90176645+Cosifran@users.noreply.github.com>
2023-04-27 18:02:50 -0500

    27/04/23 !update

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,33 +1,23 @@
-var swiper = new Swiper('.mySwiper', {
-	navigation: {
-	  nextEl: '.swiper-button-next',
-	  prevEl: '.swiper-button-prev'
-	},
-	slidesPerView: 1,
-	spaceBetween: 10,
-	// init: false,
-	pagination: {
-	  el: '.swiper-pagination',
-	  clickable: true,
-	},
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1,
+  spaceBetween: 10,
+navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+  },
+  breakpoints: {
+    640: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    768: {
+      slidesPerView: 4,
+      spaceBetween: 40,
+    },
+    1024: {
+      slidesPerView: 5,
+      spaceBetween: 50,
+    },
+  },
+});
 
-  
-	breakpoints: {
-	  620: {
-		slidesPerView: 1,
-		spaceBetween: 20,
-	  },
-	  680: {
-		slidesPerView: 2,
-		spaceBetween: 40,
-	  },
-	  920: {
-		slidesPerView: 3,
-		spaceBetween: 40,
-	  },
-	  1240: {
-		slidesPerView: 4,
-		spaceBetween: 50,
-	  },
-	} 
-    });
