a3bf65999d663bfb1bdc9c63651e61242df24c45
Francisco Front <90176645+Cosifran@users.noreply.github.com>
2023-05-18 17:58:09 -0500

    18/05/23 update

--- assets/js/app.js
+++ assets/js/app.js
@@ -21,3 +21,26 @@ navigation: {
   },
 });
 
+var swiper = new Swiper(".benificioTuSuscripcion", {
+  slidesPerView: 1,
+  spaceBetween: 10,
+navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+  },
+  breakpoints: {
+    640: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    992: {
+      slidesPerView: 3,
+      spaceBetween: 40,
+    },
+    1200: {
+      slidesPerView: 4,
+      spaceBetween: 40,
+    },
+  },
+});
+
